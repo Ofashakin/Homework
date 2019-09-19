dacd043b405436664a6f82724080a6e2a4a98cb5
Ofashakin <55443255+Ofashakin@users.noreply.github.com>
2019-09-19 08:31:40 -0400

    Add files via upload

---
+++ 2019-09-19/2019-09-19/main.cpp
@@ -0,0 +1,12 @@
+// Name: Oba-ara Fashakin
+// Intro to CS
+// Period 1
+// Mr. Williams
+// 2019-09-19-Homework
+
+#include <iostream>
+int main()
+{
+	std::cout << "Name: Oba-ara Fashakin \n Period1: Intro to CS \n Period2: Gym/ES Lab \n Period3: Health \n Period4: House \n Period5: Algebra II CC Honors \n Period6: English 10 Honors \n Period7: Lunch \n Period8: Global History 1 \n Period9: Spanish II \n Period10: Earth Science";
+
+}
